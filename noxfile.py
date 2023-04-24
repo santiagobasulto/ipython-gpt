e28a8ea6d9717565359de6df618b65de8976f1db
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-04-24 14:38:24 +0200

    make nox compatible with poetry

--- noxfile.py
+++ noxfile.py
@@ -2,8 +2,27 @@
 
 The nox run are build in isolated environment that will be stored in .nox. to force the venv update, remove the .nox/xxx folder.
 """
+import tempfile
 
 import nox
+
+
+def install_poetry_groups(session: nox.Session, *groups: str) -> None:
+    """Install dependencies from poetry groups.
+
+    Related to https://github.com/cjolowicz/nox-poetry/issues/663
+    """
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            *[f"--only={group}" for group in groups],
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("-r", requirements.name)
 
 
 @nox.session(reuse_venv=True)
@@ -11,3 +30,12 @@ def lint(session):
     """Apply the pre-commits."""
     session.install("pre-commit")
     session.run("pre-commit", "run", "--a", *session.posargs)
+
+
+@nox.session(reuse_venv=True)
+def test(session):
+    """Run all the test using the environment varialbe of the running machine."""
+    install_poetry_groups(session, "test")
+    session.install(".[test]")
+    test_files = session.posargs or ["tests"]
+    session.run("pytest", "--color=yes", *test_files)
